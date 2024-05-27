b53e00e6b644235a7fd1933bd03d9627ce1a8252
jholl1886 <150367429+jholl1886@users.noreply.github.com>
2024-05-26 22:12:27 -0400

    Add files via upload

---
+++ Items.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWS.Models
+{
+    public class Items
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+
+        public int Id { get; set; }
+
+        public int Inventory {  get; set; }
+
+        public Items()
+        {
+
+        }
+
+        public string? Display
+        {
+            get
+            {
+                return ToString();
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Description: {Description}, Price: {Price:C}, Inventory: {Inventory}";
+        }
+
+
+    }
+
+    
+
+}
---
+++ ItemsServiceProxy.cs
@@ -0,0 +1,99 @@
+﻿using AWS.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWS.library.Services
+{
+    public class ItemsServiceProxy
+    {
+        
+        private ItemsServiceProxy()
+        {
+            items = new List<Items>();
+        }
+
+        private static ItemsServiceProxy? instance;
+        private static object instanceLock = new object();
+
+        public static ItemsServiceProxy Current
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new ItemsServiceProxy();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        private List<Items>? items;
+
+        public IReadOnlyCollection<Items>? Items
+        {
+            get
+            {
+                return items?.AsReadOnly();
+            }
+        }
+
+        //functionality 
+        public int lastId
+        {
+            get
+            {
+                if (items?.Any() ?? false)
+                {
+                    return items?.Select(c => c.Id)?.Max() ?? 0;
+                }
+                return 0;
+            }
+        }
+
+        public Items? AddOrUpdate(Items item)
+        {
+            if(items == null)
+            {
+                return null;
+            }
+
+            var IsAdd = false;
+            if (item.Id == 0)
+            {
+                item.Id = lastId + 1;
+                IsAdd = true;
+            }
+            if(IsAdd)
+            {
+                items.Add(item);
+            }
+            return item;
+        }
+
+        public void Delete(int id)
+        {
+            if (items == null)
+            {
+                return;
+            }
+
+            var itemToDelete = items.FirstOrDefault(c => c.Id == id);
+
+            if(itemToDelete != null)
+            {
+                items.Remove(itemToDelete);
+            }
+
+    }
+    }
+
+   
+
+}
